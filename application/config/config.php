5e16eccdd089ac410a1b1ec8cd3fbd924c02524e
Jurgen <jurgen@agavesoft.com.mx>
2020-10-05 19:03:43 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = 'http://10.0.0.2/jauservice/';
 $config['base_url'] = 'http://localhost/jauservice/';
 */
-$config['base_url'] = 'http://wwwjauservice.com/';
+$config['base_url'] = 'http://www.jauservice.com/';
 
 /*
 |--------------------------------------------------------------------------
